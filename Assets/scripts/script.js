b58b105eb0354e5a24effdeebd492b8492061443
RossKeeley <67203345+RossKeeley@users.noreply.github.com>
2021-03-10 15:47:26 +0000

    Updated hamburger menu-mobile

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,34 +1,9 @@
-// function myFunction() {
-//   console.log("hello");
-//   var x = document.getElementById("menu");
-//   if (!x.className) {
-//     x.style.display = "none";
-//     x.classList.add("accordion");
-//     console.log("add: " + x.className);
-//   } else {
-//     x.classList.remove("accordion");
-//     x.style.display = "block";
-//     console.log("remove: " + x.className);
-//   }
-// }
-
-// function myFunction() {
-//   console.log("hello");
-//   var x = document.getElementById("menu");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//     console.log(x.style.display);
-//   } else {
-//     x.style.display = "none";
-//     console.log(x.style.display);
-//   }
-// }
-
-function myFunction() {
-  var x = document.getElementById("nav-menu");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
+function toggleFunction () {
+	var toggle = document.getElementsByClassName('toggle');
+	var menu = document.getElementById('menu-function');
+	if (menu.className === 'menu') {
+		menu.className += ' menu-mobile';
+	} else {
+		menu.className = 'menu';
+	}
 }
