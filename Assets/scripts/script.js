54bde932e6c7110b55d069a598369e23bf8ab51b
Ross Keeley <rjkeeley@gmail.com>
2021-03-10 17:29:20 +0000

    Added regex for email validator

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,3 +1,13 @@
+function searchFunction () {
+  var search = document.getElementById('search');
+  var searchBar = document.getElementById('search-bar');
+  if (searchBar.className === "search-bar") {
+    searchBar.classList.remove("search-bar");
+  } else {
+    searchBar.classList.add("search-bar");
+  }
+}
+
 function toggleFunction () {
 	var toggle = document.getElementsByClassName('toggle');
 	var menu = document.getElementById('menu-function');
@@ -9,24 +19,17 @@ function toggleFunction () {
 }
 
 function submitEmail () {
-  document.getElementById('email').value = "";
-  // if (validateEmail(input))
-  window.alert("Thank you for subscribing!");
+  var input = document.getElementById('email').value;
+  if (validateEmail(input)) {
+    window.alert("Thank you for subscribing!");
+    document.getElementById('email').value = "";
+  } else {
+    window.alert("Not a valid email. Please try again");
+    document.getElementById('email').value = "";
+  }
 }
 
 function validateEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-function searchFunction () {
-  var search = document.getElementById('search');
-  var searchBar = document.getElementById('search-bar');
-
-
-  if (searchBar.className === "search-bar") {
-    searchBar.classList.remove("search-bar");
-  } else {
-    searchBar.classList.add("search-bar");
-  }
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(email).toLowerCase());
 }
