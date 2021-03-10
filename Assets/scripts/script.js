796d8a673e6d74f21a165dca75593e36c4fadf07
Ross Keeley <rjkeeley@gmail.com>
2021-03-10 15:42:45 +0000

    Made components responsive

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,1 +1,34 @@
-﻿
+// function myFunction() {
+//   console.log("hello");
+//   var x = document.getElementById("menu");
+//   if (!x.className) {
+//     x.style.display = "none";
+//     x.classList.add("accordion");
+//     console.log("add: " + x.className);
+//   } else {
+//     x.classList.remove("accordion");
+//     x.style.display = "block";
+//     console.log("remove: " + x.className);
+//   }
+// }
+
+// function myFunction() {
+//   console.log("hello");
+//   var x = document.getElementById("menu");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//     console.log(x.style.display);
+//   } else {
+//     x.style.display = "none";
+//     console.log(x.style.display);
+//   }
+// }
+
+function myFunction() {
+  var x = document.getElementById("nav-menu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
