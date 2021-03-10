41dfcfb22e5bf7439f00a3459e3824f6e69cbc0f
Ross Keeley <rjkeeley@gmail.com>
2021-03-10 17:23:13 +0000

    Added search bar

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -18,3 +18,15 @@ function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+function searchFunction () {
+  var search = document.getElementById('search');
+  var searchBar = document.getElementById('search-bar');
+
+
+  if (searchBar.className === "search-bar") {
+    searchBar.classList.remove("search-bar");
+  } else {
+    searchBar.classList.add("search-bar");
+  }
+}
